f9ea1afe8afb4fd371c000917cf0bd06f0b96699
Sumongchelsea24 <Sujanchelsea24@gmail.com>
2026-05-27 08:26:13 +0545

    Control flow in Java example of Selection Statement

--- Selection.java
+++ Selection.java
@@ -1,16 +1,33 @@
 public class Selection {
   public static void main(String[] args) {
-    // boolean b = true;
-    int a = 10;
+    // int a = 10;
+    //  //byte--> Jvm --> machine code
+    // //Selection statement
+    // //Normal if statement
+    // if (a == 5) {
+    //   System.out.println("a is 5");
+    // }else { 
+    // System.out.println("a is not 5");     
+    // }
 
-    //Selection statement
-    //Normal if statement
-    if (a == 5) {
-      System.out.println("a is 5");
-    } else {
-      System.out.println("a is not 5");
-    }
+    //if-else- if ladder
+      // int i= 8;
+
+      // if (i == 5) {
+      //   System.out.println("i is 5");
+      // } else if (i == 6) {
+      //   System.out.println("i is 6");
+      // } else if (i == 7) {
+      //   System.out.println("i is 7");
+      // } else if (i == 8) {
+      //   System.out.println("i is 8");
+      // } else if (i == 9) {
+      //   System.out.println("i is 9");
+      // }
+
+
+    
   }
-  //byte--> Jvm --> machine code
-     
+  
+
 }
