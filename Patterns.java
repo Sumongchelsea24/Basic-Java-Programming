3fd0820e25c1f556aa4cd858b8577104e206f78a
Sumongchelsea24 <Sujanchelsea24@gmail.com>
2026-05-31 08:20:23 +0545

    More Pattern Practice

--- Patterns.java
+++ Patterns.java
@@ -43,21 +43,33 @@ public class Patterns {
     //   System.out.println();
     // } 
     //Pattern 4
-    System.out.println("\nPattern 4:");
+    // System.out.println("\nPattern 4:");
+    // for (int i = 1; i <= 5; i++) {
+    //   for (int j = 1; j <= i; j++) {
+    //     System.out.print("* ");
+    //   }
+    //   System.out.println();
+
+    // }
+    // for (int k = 1; k <= 5; k++) {
+    //   for (int l = 4; l >= k; l--) {
+    //     System.out.print("* ");
+    //   }
+    //   System.out.println();
+
+    // }
+    System.out.println("\nPattern 5:");
     for (int i = 1; i <= 5; i++) {
-      for (int j = 1; j <= i; j++) {
-        System.out.print("* ");
+      for (int j = 4; j >= i; j--) {
+        System.out.print(" ");
       }
-      System.out.println();
-
+      for (int k = 1; k <= i; k++) {
+        System.out.print("*");
+      }
+      System.out.println(" ");
+      
+      
     }
-     for (int k = 1; k <= 5; k++) {
-        for (int l = 4; l >= k; l--) {
-          System.out.print("* ");
-        }
-        System.out.println();
-
-      }
   }
   
 }
