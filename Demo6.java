109627a6c71e858372ce2a56edda5f97c7fb8de3
Sumongchelsea24 <Sujanchelsea24@gmail.com>
2026-06-06 11:13:05 +0545

    Cocept of static ,final, private in java

---
+++ Demo6.java
@@ -0,0 +1,33 @@
+public class Demo6 {
+  public static void main(String[] args) {
+    A a = new B();
+    a.fun();
+  }
+}
+//static -override hudaina--They belong to class and not to objects
+//private methods cannot be overriden
+//final  methods cannot be overriden
+
+//fields / variables they cannot be polymorphic
+class A {
+  static void fun() {
+    System.out.println("Hello");
+  }
+
+  private void fun2() {
+    System.out.println("Hello guyz");
+  }
+
+  final void fun3() {
+    System.out.println("Hello ");
+  }
+}
+
+class B extends A {
+  static void fun() {
+    System.out.println("Bye");
+  }
+  private void fun2() {
+    System.out.println("Hello everyone");
+  }
+}
