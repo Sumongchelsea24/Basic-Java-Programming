45556d701dd944bbcb19073d28a6d656fc5cc36c
Sumongchelsea24 <Sujanchelsea24@gmail.com>
2026-06-07 20:47:15 +0545

    Concept of Autoboxing and unboxing in java

--- Demo6.java
+++ Demo6.java
@@ -7,6 +7,7 @@ public class Demo6 {
 //static -override hudaina--They belong to class and not to objects
 //private methods cannot be overriden
 //final  methods cannot be overriden
+//final in class --we cannot create child of that class
 
 //fields / variables they cannot be polymorphic
 class A {
---
+++ Demo7.java
@@ -0,0 +1,40 @@
+//1.Why one public class per file.
+//2.Why name of that public class should be same as filename
+
+//Autoboxing and unboxing
+public class Demo7 {
+  public static void main(String[] args) {
+    //Autoboxing
+    // int x = 10;
+    // // Integer y = Integer.valueOf(x);
+    // Integer y = x;//autoboxing
+
+    // System.out.println(x);
+    // System.out.println(y);//unboxing
+
+    // //Unboxing
+    // Integer a = 20;//Autoboxing
+    // int b = a;//unboxing
+
+    // System.out.println(a);
+    // System.out.println(b);
+
+    // Integer x = 50;
+    // printInteger(x);
+
+    // Integer a = 10;
+    // Integer b = 20;
+    // int sum = a + b;
+    // System.out.println(sum);
+
+  }
+
+  static void printInteger(int x) {
+    System.out.println(x);
+  }
+
+  //1.Assignments
+  //2.Method calls
+  //3.Arithemtic operations
+
+}
