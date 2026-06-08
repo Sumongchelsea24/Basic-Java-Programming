b94f20f7675eb1ee544ffb34a17e19600e372cd4
Sumongchelsea24 <Sujanchelsea24@gmail.com>
2026-06-08 10:58:52 +0545

    Concept of Scanner class in Java

---
+++ Demo15.java
@@ -0,0 +1,14 @@
+//Scanner class
+import java.util.Scanner;
+public class Demo15 {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    // String name = sc.nextLine();
+    // System.out.println(name);
+    // int age = sc.nextInt();
+    // System.out.println(age);
+    double age = sc.nextDouble();
+    System.out.println(age);
+    
+  }
+}
